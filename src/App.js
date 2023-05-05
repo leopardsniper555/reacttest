59795da8d84304725ef51a462855e8364777ab81
leopardsniper555@gmail.com <leopardsniper555>
2023-05-05 03:03:00 -0700

    first to do list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{Component} from 'react';
 
-function App() {
-  return (
-    <div>
-      <h4 className = "bg-primary text-white text-center p-2">
-        To Do List  
-      </h4>
-    
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      username : "Gerard"
+    }
+  }
+  
+
+  changeStateData = () =>{
+    this.setState({
+      username:this.state.username === "Gerard" ? "Bob" : "Adam"
+    })
+  }
+
+  render = () =>
+      <div>
+        <h4 className = "bg-primary text-white text-center p-2">
+          {this.state.username}'s To Do List  
+        </h4>
+        <button className='btn btn-primary m-2' onClick={this.changeStateData}>
+          Change
+        </button>
+      
+      </div>
+  
 }
 
-export default App;
+
